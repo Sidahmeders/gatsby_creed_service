c241a5287076f895858680ad3c3146c0635f0338
sidahmed <ahorob57@gmail.com>
2020-10-16 12:38:07 +0100

    error path of filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/pages`,
+        name: `pages`
       },
     },
     `gatsby-transformer-sharp`,
